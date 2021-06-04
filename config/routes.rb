efd164943d60403ff4d5dd0a5e9d7eee38f4d5b6
tagaism <i77tag@gmail.com>
2021-06-04 16:41:06 +0000

    Upgraded ruby to v3, added bootstrap, font-awesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
   resources :courses
-  root 'static_pages#landing_page'
-  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  root 'home#index'
 end
