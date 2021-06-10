811a170f3339e0a0cb0cb2163b09456c58d305b8
tagaism <i77tag@gmail.com>
2021-06-10 17:50:23 +0000

    Styling courses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
+    @latest_courses = Course.all.order(created_at: :desc).limit(3)
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.hosts << "5a6f33deb2344d09a9032a6523185bfb.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
