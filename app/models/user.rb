1b96b5a8435abdf36bff00a7d2f586c8e696c658
tagaism <i77tag@gmail.com>
2021-06-15 15:24:02 +0000

    Add roles for users. Rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :trackable
-  
+
+  rolify
   has_many :courses
-  
+
+  after_create :assign_default_role
+
   def to_s
     email
+  end
+  
+  private
+  
+  def assign_default_role
+    self.add_role :student if self.roles.blank?
   end
 end
