4272c9896f4fa9424d72f7d827d0c8ffa9a9cfef
tagaism <i77tag@gmail.com>
2021-06-17 17:26:33 +0000

    edit user's role.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def index
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true)
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: "Role for #{@user.email} was updated successfully!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit({ role_ids: [] })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/activities'
   devise_for :users
   resources :courses
-  resources :users, only: :index
+  resources :users, only: [:index, :edit, :show, :update]
   root 'home#index'
   
 end
